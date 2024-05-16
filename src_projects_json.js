a4d59eec5b87525681115e8e7b96557e896272ac
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-05-16 18:12:16 -0400

    Deploy

---
+++ src_projects_json.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkmy_webpack_project"] = self["webpackChunkmy_webpack_project"] || []).push([["src_projects_json"],{
+
+/***/ "./src/projects.json":
+/*!***************************!*\
+  !*** ./src/projects.json ***!
+  \***************************/
+/***/ ((module) => {
+
+eval("module.exports = /*#__PURE__*/JSON.parse('[{\"title\":\"Battleship\",\"description\":\"Game inspired in gameboard Battleship\",\"preview\":\"./img/battleship.png\",\"live\":\"https://DanByteCode.github.io/Battleship-js/\",\"source\":\"https://github.com/DanByteCode/Battleship-js\",\"tools\":[\"html\",\"javascript\",\"css\",\"webpack\"]},{\"title\":\"WeatherApp\",\"description\":\"Get the weather info\",\"preview\":\"./img/Weather-App.png\",\"live\":\"https://DanByteCode.github.io/Weather-App\",\"source\":\"https://github.com/DanByteCode/Weather-App\",\"tools\":[\"html\",\"javascript\",\"css\",\"webpack\"]},{\"title\":\"Todo list\",\"description\":\"Get the weather info\",\"preview\":\"./img/Todo-list.png\",\"live\":\"https://DanByteCode.github.io/Todo-list\",\"source\":\"https://github.com/DanByteCode/Todo-list\",\"tools\":[\"html\",\"javascript\",\"css\",\"webpack\"]},{\"title\":\"Tic-Tac-Toe\",\"description\":\"Game\",\"preview\":\"./img/Tic-Tac-Toe.png\",\"live\":\"https://DanByteCode.github.io/Tic-Tac-Toe\",\"source\":\"https://github.com/DanByteCode/Tic-Tac-Toe\",\"tools\":[\"html\",\"javascript\",\"css\",\"webpack\"]},{\"title\":\"Calculator\",\"description\":\"Get the weather info\",\"preview\":\"./img/Calculator.png\",\"live\":\"https://DanByteCode.github.io/Calculator\",\"source\":\"https://github.com/DanByteCode/Calculator\",\"tools\":[\"html\",\"javascript\",\"css\",\"webpack\"]},{\"title\":\"Etch a Sketch\",\"description\":\"Get the weather info\",\"preview\":\"./img/ToP-Etch-a-Sketch.png\",\"live\":\"https://DanByteCode.github.io/ToP-Etch-a-Sketch\",\"source\":\"https://github.com/DanByteCode/ToP-Etch-a-Sketch\",\"tools\":[\"html\",\"javascript\",\"css\",\"webpack\"]}]');\n\n//# sourceURL=webpack://my-webpack-project/./src/projects.json?");
+
+/***/ })
+
+}]);
